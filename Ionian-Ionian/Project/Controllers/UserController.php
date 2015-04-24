55e2cd0bfece4bbf16f9a4cf920a7ed2525976b8
rillvar <richard.bang@live.se>
2015-04-24 13:53:01 +0200

    profile and update Action

--- Ionian-Ionian/Project/Controllers/UserController.php
+++ Ionian-Ionian/Project/Controllers/UserController.php
@@ -10,10 +10,41 @@ class UserController extends Controller {
 
     public function indexAction() {
 
-        $this->outputJSON("Congrats you successfully logged in");
     }
 
-    public function (){
+    public function profileAction(){
+
+        $db = new PDO("mysql:host=localhost;dbname=betslap", "root", "root");
+
+        $profileStm = $db->prepare('SELECT * FROM users WHERE user_id = :user_id');
+        $profileStm->bindParam(":user_id", $_SESSION["user"]->user_id, PDO::PARAM_INT);
+        $profileStm->execute();
+        $user = $profileStm->fetchObject();
+
+        require_once "profilsidan jao";
+
+        //$this->outputJSON("Congrats you successfully logged in");
     }
+
+    public function updateAction(){
+
+        $db = new PDO("mysql:host=localhost;dbname=betslap", "root", "root");
+
+        if(isset($_POST["save_button"])){
+            $updateProfileStm = $db->prepare('UPDATE users SET firstname = :firstname, lastname = :lastname, profile_pic = :p_pic, WHERE user_id = :user_id');
+            $updateProfileStm->bindParam(":user_id", $_SESSION["user"]->user_id, PDO::PARAM_INT);
+            $updateProfileStm->bindParam(":firstname", $_POST["first_name"]);
+            $updateProfileStm->bindParam(":lastname", $_POST["last_name"]);
+            $updateProfileStm->bindParam(":p_pic", $_POST["profile_pic"]);
+
+            if($updateProfileStm->execute()){
+                header("location:../users/profile");
+
+
+            }
+        }
+
+    }
+
 }
 
