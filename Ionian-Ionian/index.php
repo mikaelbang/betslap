25a4e3c92ba0b025b97a5af273abb85b77fad34d
Mikael Bang <mackeb1@gmail.com>
2015-04-24 16:00:40 +0200

    cron_job

--- Ionian-Ionian/index.php
+++ Ionian-Ionian/index.php
@@ -7,7 +7,7 @@ use Ionian\App;
 $app = new App\Rapid(App\App::APP_MODE_DEV);
 
 //You can create multiple DB instances. This however initializes the DEFAULT instance.
-$app->initDatabase("mysql", "127.0.0.1", "test", "root", "", [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false]);
+$app->initDatabase("mysql", "127.0.0.1", "betslap", "root", "root", [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false]);
 
 //By default we include the framework built-in error handler. You can include your own by using this command!
 //$app->setErrorHandler(new \Project\Handlers\CustomErrorHandler());
